007a6f9bd2e6bef458a00de0361102ef410671cd
Meera Malhotra <memalhot@redhat.com>
2026-08-24 14:49:24 -0400

    remove rdma operator checks

--- component-checks/checks.sh
+++ component-checks/checks.sh
@@ -195,7 +195,7 @@ check_deployment "gpu-operator" "nvidia-gpu-operator" "NVIDIA GPU Operator"
 check_deployment "knative-openshift" "openshift-serverless" "OpenShift Serverless"
 check_deployment "istio-operator" "openshift-operators" "OpenShift Service Mesh"
 check_deployment "nvidia-network-operator-controller-manager" "nvidia-network-operator" "NVIDIA Network Operator"
-check_deployment "sriov-network-operator" "openshift-sriov-network-operator" "SR-IOV Network Operator"
+# check_deployment "sriov-network-operator" "openshift-sriov-network-operator" "SR-IOV Network Operator"
 
 # --- Configuration & Component Readiness ---
 
