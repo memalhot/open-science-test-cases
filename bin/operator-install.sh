7c78ca8fa07a27680526e4991d87982973715c3d
Meera Malhotra <memalhot@redhat.com>
2026-09-01 11:32:03 -0400

    add operator config for kubeflow trainer

--- bin/operator-install.sh
+++ bin/operator-install.sh
@@ -1,7 +1,16 @@
+# for kagenti
 helm install rossoctl-operator \
   oci://ghcr.io/rossoctl/operator/operator-chart \
   --namespace rossoctl-system \
   --create-namespace \
   --set controllerManager.container.image.tag=0.3.0 \
   --kube-as-user=system:
+
+# for 
+
+kubectl apply --server-side -k "github.com/kubeflow/training-operator.git/manifests/overlays/standalone?ref=v1.8.1"
+
+oc set image deployment/training-operator  -n kubeflow training-operator=docker.io/kubeflow/training-operator:v1-04f9f13
+
+oc rollout status deployment/training-operator -n kubeflow
   
