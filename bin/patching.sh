9907f6e216faa16cd2b7d35d8d088643838d509e
Meera Malhotra <memalhot@redhat.com>
2026-09-11 11:40:41 -0400

    rwx patch to show rwx storage as an option on the openshift ai ui

--- bin/patching.sh
+++ bin/patching.sh
@@ -11,3 +11,8 @@ oc patch odhdashboardconfig odh-dashboard-config \
   --type merge \
   -p '{"spec":{"dashboardConfig":{"disableHardwareProfiles":false}}}' \
   --as system:admin
+
+oc annotate storageclass pure-fb-nfsv4 \
+  --as system:admin \
+  --overwrite \
+  "opendatahub.io/sc-config={\"displayName\":\"pure-fb-nfsv4\",\"isEnabled\":true,\"isDefault\":true,\"lastModified\":\"$(date -u +%Y-%m-%dT%H:%M:%SZ)\",\"accessModeSettings\":{\"ReadWriteOnce\":true,\"ReadWriteMany\":true,\"ReadOnlyMany\":false,\"ReadWriteOncePod\":false}}"
